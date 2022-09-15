6fa849d1fe82035f6d9fc9094ac9569e7e9c584a
uyuyorum {um} <56808589+alp1x@users.noreply.github.com>
2022-09-15 17:35:32 +0300

    blank

--- server/server.lua
+++ server/server.lua
@@ -1,3 +1,3 @@
 RegisterNetEvent("um-truthordare:server:rotate", function(x,y,z,number)
-    TriggerClientEvent('um-truthordare:client:rotate', -1, x,y,z,number)
+     TriggerClientEvent('um-truthordare:client:rotate', -1, x,y,z,number)
 end)
